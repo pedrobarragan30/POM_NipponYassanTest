7b9930e14b643acf628237948fd20ad5d78c9e0a
PedroB <pedrobarragan30@gmail.com>
2018-06-07 23:01:00 -0500

    Add Home Page

--- src/pages/HomePage.java
+++ src/pages/HomePage.java
@@ -1,6 +1,9 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
@@ -12,4 +15,13 @@ public class HomePage {
 		waitHomePage = new WebDriverWait(driver,15);
 	}
 
+	public void search(String searchTerm) {
+		WebElement searchField = waitHomePage.until(ExpectedConditions.presenceOfElementLocated(By.id("search_query")));
+		searchField.clear();
+		searchField.sendKeys(searchTerm);
+		
+		WebElement searchButton = waitHomePage.until(ExpectedConditions.presenceOfElementLocated(By.id("search_button")));
+		waitHomePage.until(ExpectedConditions.elementToBeClickable(By.id("search_button")));
+		searchButton.click();		
+	}
 }
